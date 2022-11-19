65bebd621c9441aa569bb6218470dcbbbc94751d
Krzysztof Baran <krysbaran@gmail.com>
2022-11-19 14:52:46 +0100

    Correct one of the pages

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 import Layout from '../components/layout'
 import Container from 'react-bootstrap/Container'
-import SEO from '../components/Seo'
+import SEO from '../components/seo'
 
 export default () => (
   <Layout>
